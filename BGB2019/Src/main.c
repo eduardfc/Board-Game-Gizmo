0686fb4dd455a450db030b8a3eebe1d5e506b69d
Eduardo Castro <edfcastro@gmail.com>
2020-06-27 20:01:17 -0300

    cleam some remarks3

--- BGB2019/Src/main.c
+++ BGB2019/Src/main.c
@@ -98,6 +98,7 @@ unsigned char LIXO1 = 1; //lixo
 unsigned char LIXO2 = 1; //lixo
 unsigned short V_BATT;
 
+
 const char MHOME1[6][11]={"HOME MENU", "TIMER","CHRONO","SCOREBOARD", "CHESSCLOCK","     MORE>"}; //itens for mode APP menu
 const char MHOME2[6][11]={"HOME MENU", "HOURGLASS", "B I N G O","ROULETTE","","     MORE>"}; //itens for mode APP menu
 
@@ -457,7 +458,7 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 35999;
+  htim4.Init.Period = 36002;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
